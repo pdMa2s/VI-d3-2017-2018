fe51200859534e597c656aa63aef23266f5accd0
Pedro Matos <pedroguilhermematos@ua.pt>
2018-01-04 22:12:36 +0000

    chart

--- dataHandler.py
+++ dataHandler.py
@@ -3,5 +3,7 @@ import pandas as pd
 all_data = pd.read_csv("globalterroris.csv", encoding = "ISO-8859-1");
 
 relevant_data =  all_data[['eventid','iyear', 'country_txt', 'latitude', 'longitude', 'nkill']]
-print(relevant_data)
-relevant_data.to_csv("relevant_terrorist.csv",index=False)
+#print(relevant_data)
+#relevant_data.to_csv("relevant_terrorist.csv",index=False)
+
+print(relevant_data.groupby(['country_txt','iyear']).size())
